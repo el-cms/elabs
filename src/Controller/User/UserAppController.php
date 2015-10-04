512b7ab3ff3f26d14265d3ec4bd34ed56b0bcbaf
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-10-04 16:08:45 +0200

    Added UserAppController

---
+++ src/Controller/User/UserAppController.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Controller\Admin;
+
+use App\Controller\AppController;
+use Cake\Event\Event;
+
+/**
+ * User controller. All the admin controllers inherits from it.
+ */
+class UserAppController extends AppController {
+
+	/**
+	 * Before render callback.
+	 *
+	 * @param \Cake\Event\Event $event The beforeRender event.
+	 * @return void
+	 */
+	public function beforeRender(Event $event) {
+		parent::beforeRender($event);
+		$this->viewBuilder()->layout('admin');
+	}
+
+}
