ad75f53da0114d7d4203a2446c404a94840b2a32
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-10-04 16:10:45 +0200

    Added custom helper : Boostrap

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * CakePHP(tm) : Rapid Development Framework (http://cakephp.org)
  * Copyright (c) Cake Software Foundation, Inc. (http://cakefoundation.org)
@@ -12,6 +13,7 @@
  * @since     0.2.9
  * @license   http://www.opensource.org/licenses/mit-license.php MIT License
  */
+
 namespace App\Controller;
 
 use Cake\Controller\Controller;
@@ -25,38 +27,47 @@ use Cake\Event\Event;
  *
  * @link http://book.cakephp.org/3.0/en/controllers.html#the-app-controller
  */
-class AppController extends Controller
-{
+class AppController extends Controller {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * e.g. `$this->loadComponent('Security');`
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-        parent::initialize();
+	/**
+	 * Helpers
+	 * 
+	 * @var type array
+	 */
+	public $helpers = [
+			'Form' => [
+					'className' => 'Bootstrap.BootstrapForm' // instead of 'Bootstrap3.BootstrapForm'
+			]
+	];
 
-        $this->loadComponent('RequestHandler');
-        $this->loadComponent('Flash');
-    }
+	/**
+	 * Initialization hook method.
+	 *
+	 * Use this method to add common initialization code like loading components.
+	 *
+	 * e.g. `$this->loadComponent('Security');`
+	 *
+	 * @return void
+	 */
+	public function initialize() {
+		parent::initialize();
 
-    /**
-     * Before render callback.
-     *
-     * @param \Cake\Event\Event $event The beforeRender event.
-     * @return void
-     */
-    public function beforeRender(Event $event)
-    {
-        if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
-        ) {
-            $this->set('_serialize', true);
-        }
-    }
+		$this->loadComponent('RequestHandler');
+		$this->loadComponent('Flash');
+	}
+
+	/**
+	 * Before render callback.
+	 *
+	 * @param \Cake\Event\Event $event The beforeRender event.
+	 * @return void
+	 */
+	public function beforeRender(Event $event) {
+		if (!array_key_exists('_serialize', $this->viewVars) &&
+						in_array($this->response->type(), ['application/json', 'application/xml'])
+		) {
+			$this->set('_serialize', true);
+		}
+	}
+
 }
