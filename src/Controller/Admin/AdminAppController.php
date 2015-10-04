969f70f7c16cd941407a784b77a35276a18971f7
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-10-04 16:09:22 +0200

    Updated AdminAppController to extend AppController instead of Controller

--- src/Controller/Admin/AdminAppController.php
+++ src/Controller/Admin/AdminAppController.php
@@ -2,45 +2,23 @@
 
 namespace App\Controller\Admin;
 
-use Cake\Controller\Controller;
+use App\Controller\AppController;
 use Cake\Event\Event;
 
 /**
  * Admin controller. All the admin controllers inherits from it.
  */
-class AdminAppController extends Controller
-{
+class AdminAppController extends AppController {
 
-    /**
-     * Initialization hook method.
-     *
-     * Use this method to add common initialization code like loading components.
-     *
-     * e.g. `$this->loadComponent('Security');`
-     *
-     * @return void
-     */
-    public function initialize()
-    {
-        parent::initialize();
+	/**
+	 * Before render callback.
+	 *
+	 * @param \Cake\Event\Event $event The beforeRender event.
+	 * @return void
+	 */
+	public function beforeRender(Event $event) {
+		parent::beforeRender($event);
+		$this->viewBuilder()->layout('admin');
+	}
 
-        $this->loadComponent('RequestHandler');
-        $this->loadComponent('Flash');
-    }
-
-    /**
-     * Before render callback.
-     *
-     * @param \Cake\Event\Event $event The beforeRender event.
-     * @return void
-     */
-    public function beforeRender(Event $event)
-    {
-        if (!array_key_exists('_serialize', $this->viewVars) &&
-            in_array($this->response->type(), ['application/json', 'application/xml'])
-        ) {
-            $this->set('_serialize', true);
-        }
-				$this->viewBuilder()->layout('admin');
-    }
 }
