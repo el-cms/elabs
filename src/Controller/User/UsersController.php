cc397984f3a0b75eefa05e0e138e80db80881f11
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-10-10 05:05:46 +0200

    Generated admin/user User controllers

---
+++ src/Controller/User/UsersController.php
@@ -0,0 +1,66 @@
+<?php
+namespace App\Controller\User;
+
+use App\Controller\User\UserAppController;
+
+/**
+ * Users Controller
+ *
+ * @property \App\Model\Table\UsersTable $Users
+ */
+class UsersController extends UserAppController
+{
+
+    /**
+     * Index method
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $this->set('users', $this->paginate($this->Users));
+        $this->set('_serialize', ['users']);
+    }
+
+    /**
+     * View method
+     *
+     * @param string|null $id User id.
+     * @return void
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function view($id = null)
+    {
+        $user = $this->Users->get($id, [
+            'contain' => ['Events', 'Posts', 'Projects']
+        ]);
+        $this->set('user', $user);
+        $this->set('_serialize', ['user']);
+    }
+
+    /**
+     * Edit method
+     *
+     * @param string|null $id User id.
+     * @return void Redirects on successful edit, renders view otherwise.
+     * @throws \Cake\Network\Exception\NotFoundException When record not found.
+     */
+    public function edit()
+    {
+        $user = $this->Users->get($this->Auth->user('id'), [
+            'contain' => []
+        ]);
+        if ($this->request->is(['patch', 'post', 'put'])) {
+            $user = $this->Users->patchEntity($user, $this->request->data);
+            if ($this->Users->save($user)) {
+                $this->Flash->success(__('The user has been saved.'));
+                return $this->redirect(['action' => 'index']);
+            } else {
+                $this->Flash->error(__('The user could not be saved. Please, try again.'));
+            }
+        }
+        $this->set(compact('user'));
+        $this->set('_serialize', ['user']);
+    }
+
+}
