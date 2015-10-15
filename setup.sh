660b5a3aace726e78291fdd616b3ab426b96da78
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-10-15 23:23:17 +0200

    Added setup.sh Run setup.sh to download/extract non-composer plugins and reload the Composer autoloader

---
+++ setup.sh
@@ -0,0 +1,25 @@
+#!/bin/bash -   
+#title          :setup.sh
+#description    :Downloads extra plugins not available with Composer
+#author         :Manuel Tancoigne
+#date           :20151015
+#version        :0      
+#usage          :./setup.sh
+#notes          :       
+#bash_version   :4.3.30(1)-release
+#============================================================================
+
+# Gravatar plugin
+cd plugins
+wget https://github.com/el-cms/cakephp-gravatar-plugin/archive/Cake-3.X.zip
+unzip Cake-3.X.zip
+mv cakephp-gravatar-plugin-Cake-3.X Gravatar
+rm Cake-3.X.zip
+#./../bin/cake plugin load Gravatar
+
+# Update autoloader
+cd ../
+composer dumpautoload
+
+echo "\n\nDone\n\n"
+
