559d33b0493b5d2e34354525e795fca733d7fe62
Manuel Tancoigne <m.tancoigne@gmail.com>
2015-11-18 13:16:56 +0100

    Updated gitignore and created a bash script to download sources

---
+++ Sources/Sass/setup.sh
@@ -0,0 +1,14 @@
+cd common
+
+wget https://github.com/Daemonite/material/archive/master.zip
+unzip master.zip
+rm master.zip -f
+
+wget https://github.com/FortAwesome/Font-Awesome/archive/master.zip
+unzip master.zip
+rm master.zip -f
+
+wget http://codemirror.net/codemirror.zip
+unzip codemirror.zip
+rm codemirror.zip -f
+mv codemirror-* codemirror 
