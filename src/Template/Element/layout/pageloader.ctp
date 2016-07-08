c3e11c9d3ec754ac0184e5159ed7712b9a1373a7
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-07-08 10:38:27 +0200

    Updated page loader

--- src/Template/Element/layout/pageloader.ctp
+++ src/Template/Element/layout/pageloader.ctp
@@ -1,15 +1,5 @@
-<div class="avoid-fout-indicator avoid-fout-indicator-fixed">
-	<div class="progress-circular progress-circular-alt progress-circular-center">
-		<div class="progress-circular-wrapper">
-			<div class="progress-circular-inner">
-				<div class="progress-circular-left">
-					<div class="progress-circular-spinner"></div>
-				</div>
-				<div class="progress-circular-gap"></div>
-				<div class="progress-circular-right">
-					<div class="progress-circular-spinner"></div>
-				</div>
-			</div>
-		</div>
+<div id="page-loader">
+	<div class="loader-centered">
+		<i class="fa fa-spin fa-spinner fa-4x" title="Page is loading"></i>
 	</div>
 </div>
--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -1,24 +1,6 @@
-// ui-progress.html
-		$('.finish-loading').on('click', function(e) {
-			e.stopPropagation();
-			$($(this).attr('data-target')).addClass('el-loading-done');
-		});
-
-		$('#el-loading-tile-wrap .tile-active-show').each(function (index) {
-			var $this = $(this),
-			    timer;
-
-			$this.on('hide.bs.tile', function(e) {
-				clearTimeout(timer);
-			});
-
-			$this.on('show.bs.tile', function(e) {
-				if (!$('.el-loading', $this).hasClass('el-loading-done')) {
-					timer = setTimeout(function() {
-						$('.el-loading', $this).addClass('el-loading-done');
-						$this.prepend('<div class="tile-sub"><p>Additional information<br><small>Aliquam in pharetra leo. In congue, massa sed elementum dictum, justo quam efficitur risus, in posuere mi orci ultrices diam.</small></p></div>');
-					}, 6000);
-				};
-			});
-		});
- 
+// Page loader script
+// http://bootdey.com/snippets/view/page-loader
+jQuery(window).load(function() {
+    //$(".loader-centered").fadeOut();
+    $("#page-loader").delay(400).fadeOut("slow");
+});
