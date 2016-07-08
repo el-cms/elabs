a845b1cee2f439a40b0195d8f292a2ebfddd76dd
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-07-08 13:23:53 +0200

    Removed comments

--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -1,6 +1,5 @@
 // Page loader script
 // http://bootdey.com/snippets/view/page-loader
 jQuery(window).load(function() {
-    //$(".loader-centered").fadeOut();
     $("#page-loader").delay(400).fadeOut("slow");
 });
