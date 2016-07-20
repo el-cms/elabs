a4aab390cfeb73cf11e89133276d519c3dba8aec
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-07-21 01:48:46 +0200

    Forgot to update the baked fixtures

--- tests/Fixture/LanguagesFixture.php
+++ tests/Fixture/LanguagesFixture.php
@@ -19,6 +19,9 @@ class LanguagesFixture extends TestFixture
     public $fields = [
         'id' => ['type' => 'string', 'fixed' => true, 'length' => 4, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null],
         'name' => ['type' => 'string', 'length' => 45, 'null' => false, 'default' => null, 'collate' => 'utf8_general_ci', 'comment' => '', 'precision' => null, 'fixed' => null],
+        'post_count' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'project_count' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
+        'file_count' => ['type' => 'integer', 'length' => 11, 'unsigned' => false, 'null' => false, 'default' => '0', 'comment' => '', 'precision' => null, 'autoIncrement' => null],
         '_constraints' => [
             'primary' => ['type' => 'primary', 'columns' => ['id'], 'length' => []],
         ],
@@ -36,8 +39,11 @@ class LanguagesFixture extends TestFixture
      */
     public $records = [
         [
-            'id' => 'fbdcb80f-9e5d-4c02-95f6-1238f8477570',
-            'name' => 'Lorem ipsum dolor sit amet'
+            'id' => '2f5ec13e-9253-47c6-81b6-6ed7f200add4',
+            'name' => 'Lorem ipsum dolor sit amet',
+            'post_count' => 1,
+            'project_count' => 1,
+            'file_count' => 1
         ],
     ];
 }
