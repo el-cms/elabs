3643e3aeb0d60e7d17468880904e7526c28dbf57
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-07-23 16:17:49 +0200

    Changed the way big images are displayed

--- src/Template/Element/files/view_content_image.ctp
+++ src/Template/Element/files/view_content_image.ctp
@@ -1,4 +1,3 @@
-<div class="img-cropped-wrapper">
-    <div class="small-overlay"></div>
+<div>
     <?php echo $this->Html->image('../uploads/' . $data['filename'], ['alt' => h($data['title']), 'class' => 'img-responsive']) ?>
 </div>
--- webroot/js/custom.js
+++ webroot/js/custom.js
@@ -1,5 +1,17 @@
 // Page loader script
 // http://bootdey.com/snippets/view/page-loader
-jQuery(window).load(function() {
-    $("#page-loader").delay(400).fadeOut("slow");
+jQuery(window).load(function () {
+  // Loader
+  $("#page-loader").delay(400).fadeOut("slow");
+  // Wrapper for big images
+  $(".card-content img").each(function (i, e) {
+    $e = $(e);
+    if ($e.height() > 300) {
+      $e.parent().addClass('img-cropped-wrapper');
+    }
+  });
+  // Resize images
+  $('.img-cropped-wrapper').click(function () {
+    $(this).toggleClass('in');
+  });
 });
