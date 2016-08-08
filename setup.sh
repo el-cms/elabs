febc3a426ea43de9f854d320b6fda3058c1e0715
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-08-08 20:11:23 +0200

    Minor fixes

--- setup.sh
+++ setup.sh
@@ -22,7 +22,7 @@ ELABS_DIR=`pwd`
 popd > /dev/null
 # Make ELABS_DIR available in other scripts
 export ELABS_DIR
-
+echo ""
 echo -e "${EL_BOX_TOP_LINE}";
 echo -e "+-------------------------------------------------------------------+${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
@@ -33,10 +33,10 @@ echo -e "+- - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -+${
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
 echo -e "| \e[34mNow, the installer will download some js and css files.\e[39m           |${EL_BOX_SHADOW_LIGHT}"
 echo -e "| -------------------------------------------------------           |${EL_BOX_SHADOW_LIGHT}"
-
 # Css/JS sources
 ./Sources/Sass/setup.sh;
 
+echo "";
 echo -e "${EL_BOX_TOP_LINE}";
 echo -e "+-------------------------------------------------------------------+${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
@@ -67,11 +67,12 @@ elif [ 'n' == "$INSTALLDEV" ]; then
     composer install --no-dev;
 fi;
 
+echo "";
 echo -e "${EL_BOX_TOP_LINE}";
 echo -e "+-------------------------------------------------------------------+${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34mTake some time to set up your application\e[39m                     |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34m-----------------------------------------\e[39m                     |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34mTake some time to set up your application\e[39m                         |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34m-----------------------------------------\e[39m                         |${EL_BOX_SHADOW_LIGHT}"
 echo -e "| The next step will be the database setup.                         |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|   - Edit the 'config/app.php' file and enter the proper database  |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|     configuration values.                                         |${EL_BOX_SHADOW_LIGHT}"
@@ -89,18 +90,19 @@ read OK;
 echo -e "${EL_BOX_TOP_LINE}";
 echo -e "+-------------------------------------------------------------------+${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34mPopulating database...\e[39m                                        |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34m----------------------\e[39m                                        |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34mPopulating database...\e[39m                                            |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34m----------------------\e[39m                                            |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
 echo -e "+-------------------------------------------------------------------+";
 echo ""
 bin/cake Migrations migrate
 bin/cake Migrations seed
 
+echo ""
 echo -e "+-------------------------------------------------------------------+${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34mSetup is over.\e[39m                                                |${EL_BOX_SHADOW_LIGHT}"
-echo -e "| \e[34m--------------\e[39m                                                |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34mSetup is over.\e[39m                                                    |${EL_BOX_SHADOW_LIGHT}"
+echo -e "| \e[34m--------------\e[39m                                                    |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|   You can now log in using the following credentials:             |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|     - email: admin@example.com                                    |${EL_BOX_SHADOW_LIGHT}"
@@ -112,5 +114,3 @@ echo -e "|   - Issues: https://github.com/el-cms/elabs/issues                |${
 echo -e "|   - News: http://experimentslabs.com                              |${EL_BOX_SHADOW_LIGHT}"
 echo -e "|                                                                   |${EL_BOX_SHADOW_LIGHT}"
 echo -e "+-------------------------------------------------------------------+";
-
-
