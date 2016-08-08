3f2ef5b8318988470aba496605194f7bf9824665
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-08-08 08:34:57 +0200

    Updated time() to frozenTime()

--- src/View/Helper/ElabsTimeHelper.php
+++ src/View/Helper/ElabsTimeHelper.php
@@ -2,7 +2,7 @@
 
 namespace App\View\Helper;
 
-use Cake\I18n\Time;
+use Cake\I18n\FrozenTime;
 
 /**
  * CakePHP Helper
@@ -14,12 +14,12 @@ class ElabsTimeHelper extends \Cake\View\Helper\TimeHelper
     /**
      * Compares two dates and returns true if they are the same day.
      *
-     * @param Time $date1 First date
-     * @param Time $date2 Second date
+     * @param Cake\I18n\FrozenTime $date1 First date
+     * @param Cake\I18n\FrozenTime $date2 Second date
      *
      * @return bool
      */
-    public function isSameDay(Time $date1, Time $date2)
+    public function isSameDay(FrozenTime $date1, FrozenTime $date2)
     {
         if ($date1->diffInDays($date2) > 0) {
             return false;
