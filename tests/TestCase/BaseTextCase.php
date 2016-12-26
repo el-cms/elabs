d88adfad7b73b976b13ac2bc523cf320767bc8f0
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-12-26 09:13:35 +0100

    Added methods to test rights accesses for each roles on a given action/method

--- tests/TestCase/BaseTextCase.php
+++ tests/TestCase/BaseTextCase.php
@@ -9,6 +9,7 @@
 namespace App\Test\TestCase;
 
 use Cake\TestSuite\IntegrationTestCase;
+use \App\Test\Fixture\UsersFixture;
 
 class BaseTextCase extends IntegrationTestCase
 {
@@ -23,19 +24,101 @@ class BaseTextCase extends IntegrationTestCase
      * Constructor that loads some fixtures data for simpler reference
      *
      * @param string $name Name
-     * @param array $data  Data
+     * @param array $data Data
      * @param string $dataName Data name
      */
     public function __construct($name = null, array $data = [], $dataName = '')
     {
         parent::__construct($name, $data, $dataName);
-        $userFixtures = new \App\Test\Fixture\UsersFixture();
+        $userFixtures = new UsersFixture();
+
         $this->userCreds = [
-            'closed' => ['Auth' => ['User' => $userFixtures->records[0]]],
+//            'closed' => ['Auth' => ['User' => $userFixtures->records[0]]],
             'admin' => ['Auth' => ['User' => $userFixtures->records[1]]],
             'author' => ['Auth' => ['User' => $userFixtures->records[2]]],
-            'inactive' => ['Auth' => ['User' => $userFixtures->records[3]]],
-            'locked' => ['Auth' => ['User' => $userFixtures->records[4]]],
+//            'inactive' => ['Auth' => ['User' => $userFixtures->records[3]]],
+//            'locked' => ['Auth' => ['User' => $userFixtures->records[4]]],
+            'public' => ['Auth' => ['User' => []]],
         ];
+    }
+
+    /**
+     * Testes authorisation on a page that don't redirects the user on success
+     *
+     * @param array $statuses Users to check
+     * @param string $path URL
+     * @param array $methods Methods (post/get for now)
+     *
+     * @return void
+     */
+    public function assertAuthIsOkFor($statuses = [], string $path, $methods = [])
+    {
+        foreach ($this->userCreds as $status => $credentials) {
+            if (in_array($status, $statuses)) {
+                $this->assertAccessOk($credentials, $path, $methods, "access for '$status' ($path) failed.");
+            } else {
+                $this->assertAccessFails($credentials, $path, $methods, "access denial for '$status' ($path) failed.");
+            }
+        }
+    }
+
+    /**
+     * Testes a successful action for a given user
+     *
+     * @param array $session User configuration value for Auth
+     * @param string $path URL to check
+     * @param array $methods Methods to check (post, get)
+     * @param string|null $message Failure message. Will be preceded with the method
+     *
+     * @return void
+     */
+    public function assertAccessOk(array $session, string $path, array $methods, string $message = null)
+    {
+        $this->session($session);
+        foreach ($methods as $m) {
+            try {
+                switch ($m) {
+                    case 'get':
+                        $this->get($path);
+                        break;
+                    case 'post':
+                        $this->post($path);
+                        break;
+                }
+                $this->assertResponseOk();
+            } catch (\PHPUnit_Framework_ExpectationFailedException $e) {
+                $this->fail($e->getMessage() . "\n > $m - $message");
+            }
+        }
+    }
+
+    /**
+     * Testes a denied action for a given user
+     *
+     * @param array $session User configuration value for Auth
+     * @param string $path URL to check
+     * @param array $methods Methods to check (post, get)
+     * @param string|null $message Failure message. Will be preceded with the method
+     *
+     * @return void
+     */
+    public function assertAccessFails(array $session, string $path, array $methods, string $message = null)
+    {
+        $this->session($session);
+        foreach ($methods as $m) {
+            try {
+                switch ($m) {
+                    case 'get':
+                        $this->get($path);
+                        break;
+                    case 'post':
+                        $this->post($path);
+                        break;
+                }
+                $this->assertResponseError();
+            } catch (\PHPUnit_Framework_ExpectationFailedException $e) {
+                $this->fail($e->getMessage() . "\n > $message");
+            }
+        }
     }
 }
