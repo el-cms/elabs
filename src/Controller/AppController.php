a26bfa9028894be5ffd636c67137bd10706b6f98
Manuel Tancoigne <m.tancoigne@gmail.com>
2016-12-26 12:31:58 +0100

    Added a config file an enabled plugin

--- src/Controller/AppController.php
+++ src/Controller/AppController.php
@@ -17,6 +17,7 @@
 namespace App\Controller;
 
 use Cake\Controller\Controller;
+use Cake\Core\Configure;
 use Cake\Event\Event;
 use Cake\I18n\I18n;
 
@@ -47,19 +48,7 @@ class AppController extends Controller
         $this->loadComponent('RequestHandler');
         $this->loadComponent('Act');
         $this->loadComponent('Flash');
-        $this->loadComponent('Auth', [
-            'authenticate' => [
-                'Form' => [
-                    'fields' => ['username' => 'email'],
-                    'scope' => ['status' => STATUS_ACTIVE],
-                ],
-            ],
-            'loginAction' => ['prefix' => false, 'controller' => 'Users', 'action' => 'login'],
-            'authError' => __d('elabs', 'You are not allowed to view this page.'),
-            'flash' => ['key' => 'flash', 'params' => ['class' => 'danger alert']],
-            'loginRedirect' => ['prefix' => false, 'controller' => 'acts', 'action' => 'index'],
-            'logoutRedirect' => ['prefix' => false, 'controller' => 'acts', 'action' => 'index']
-        ]);
+        $this->loadComponent('CakeDC/Users.UsersAuth');
 
         /*
          * Enable the following components for recommended CakePHP security settings.
