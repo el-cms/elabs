fd0a553f3ec27a3e58a948bb84e1df63d83149f5
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-01-03 03:57:24 +0100

    Removed forgotten comment

--- src/Controller/LicensesController.php
+++ src/Controller/LicensesController.php
@@ -40,62 +40,7 @@ class LicensesController extends AppController
     {
 
         $query = $this->Licenses->getWithContain($id, ['sfw' => !$this->seeNSFW]);
-//        $seeNSFW = $this->seeNSFW;
-//        $contains = [
-//            'Languages' => ['fields' => ['id', 'iso639_1', 'name']],
-//            'Users' => ['fields' => ['id', 'username']]
-//        ];
-//        $query = $this->Licenses->find();
-//        $query->select()
-//                ->where(['Licenses.id' => $id])
-//                ->contain([
-//                    'Posts' => function ($q) use ($contains, $seeNSFW) {
-//                        $q = $q->select(['id', 'title', 'excerpt', 'modified', 'publication_date', 'sfw', 'user_id', 'license_id', 'language_id'])
-//                                ->where(['Posts.status' => STATUS_PUBLISHED])
-//                                ->contain($contains)
-//                                ->limit(Configure::read('cms.maxRelatedData'));
-//                        if (!$seeNSFW) {
-//                            $q = $q->where(['Posts.sfw' => true]);
-//                        }
-//
-//                        return $q;
-//                    },
-//                    'Notes' => function ($q) use ($contains, $seeNSFW) {
-//                        $q = $q->select(['id', 'text', 'sfw', 'modified', 'created', 'user_id', 'license_id', 'language_id'])
-//                                ->where(['Notes.status' => STATUS_PUBLISHED])
-//                                ->contain($contains)
-//                                ->limit(Configure::read('cms.maxRelatedData'));
-//                        if (!$seeNSFW) {
-//                            $q = $q->where(['Notes.sfw' => true]);
-//                        }
-//
-//                        return $q;
-//                    },
-//                    'Projects' => function ($q) use ($contains, $seeNSFW) {
-//                        $q = $q->select(['id', 'name', 'short_description', 'sfw', 'created', 'modified', 'user_id', 'license_id', 'language_id'])
-//                                ->where(['Projects.status' => STATUS_PUBLISHED])
-//                                ->contain($contains)
-//                                ->limit(Configure::read('cms.maxRelatedData'));
-//                        if (!$seeNSFW) {
-//                            $q = $q->where(['Projects.sfw' => true]);
-//                        }
-//
-//                        return $q;
-//                    },
-//                    'Files' => function ($q) use ($contains, $seeNSFW) {
-//                        $q = $q->select(['id', 'name', 'description', 'filename', 'sfw', 'created', 'modified', 'user_id', 'license_id', 'language_id'])
-//                                ->where(['Files.status' => STATUS_PUBLISHED])
-//                                ->contain($contains)
-//                                ->limit(Configure::read('cms.maxRelatedData'));
-//                        if (!$seeNSFW) {
-//                            $q = $q->where(['Files.sfw' => true]);
-//                        }
-//
-//                        return $q;
-//                    },
-//                ]);
-//
-//        $license = $query->firstOrFail();
+        
         $this->set('license', $query);
         $this->set('_serialize', ['license']);
     }
