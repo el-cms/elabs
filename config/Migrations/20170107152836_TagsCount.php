210e368d7fa8dc77018711bb91fa1f6019fc7731
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-01-07 21:17:12 +0100

    Fixed last migrations (changes order were wrong)

--- config/Migrations/20170107152836_TagsCount.php
+++ config/Migrations/20170107152836_TagsCount.php
@@ -9,15 +9,11 @@ class TagsCount extends AbstractMigration
     {
 
         $this->table('tags')
-                ->removeColumn('itemtag_count')
-                ->update();
-
-        $this->table('tags')
                 ->addColumn('album_count', 'integer', ['after' => 'id', 'default' => '0', 'length' => 5, 'null' => false])
-                ->addColumn('file_count', 'integer', ['after' => 'file_count', 'default' => '0', 'length' => 5, 'null' => false])
-                ->addColumn('note_count', 'integer', ['after' => 'note_count', 'default' => '0', 'length' => 5, 'null' => false])
-                ->addColumn('post_count', 'integer', ['after' => 'post_count', 'default' => '0', 'length' => 5, 'null' => false])
-                ->addColumn('project_count', 'integer', ['after' => 'project_count', 'default' => '0', 'length' => 5, 'null' => false])
+                ->addColumn('file_count', 'integer', ['after' => 'album_count', 'default' => '0', 'length' => 5, 'null' => false])
+                ->addColumn('note_count', 'integer', ['after' => 'file_count', 'default' => '0', 'length' => 5, 'null' => false])
+                ->addColumn('post_count', 'integer', ['after' => 'note_count', 'default' => '0', 'length' => 5, 'null' => false])
+                ->addColumn('project_count', 'integer', ['after' => 'post_count', 'default' => '0', 'length' => 5, 'null' => false])
                 ->update();
     }
 
@@ -25,7 +21,6 @@ class TagsCount extends AbstractMigration
     {
 
         $this->table('tags')
-                ->addColumn('itemtag_count', 'integer', ['after' => 'id', 'default' => '0', 'length' => 5, 'null' => false])
                 ->removeColumn('album_count')
                 ->removeColumn('file_count')
                 ->removeColumn('note_count')
