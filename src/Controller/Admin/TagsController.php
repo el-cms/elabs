47a929aaf9e6deba31d2b607a5bce8b8777f741d
Manuel Tancoigne <m.tancoigne@gmail.com>
2017-01-11 21:00:31 +0100

    Created an admin page for tags

--- src/Controller/Admin/TagsController.php
+++ src/Controller/Admin/TagsController.php
@@ -2,7 +2,7 @@
 
 namespace App\Controller\Admin;
 
-use App\Controller\AdminAppController;
+use App\Controller\Admin\AdminAppController;
 
 /**
  * Tags Controller
@@ -23,79 +23,6 @@ class TagsController extends AdminAppController
 
         $this->set(compact('tags'));
         $this->set('_serialize', ['tags']);
-    }
-
-    /**
-     * View method
-     *
-     * @param string|null $id Tag id.
-     * @return \Cake\Network\Response|void
-     * @throws \Cake\Datasource\Exception\RecordNotFoundException When record not found.
-     */
-    public function view($id = null)
-    {
-        $tag = $this->Tags->get($id, [
-            'contain' => ['Files', 'Notes', 'Posts', 'Projects']
-        ]);
-
-        $this->set('tag', $tag);
-        $this->set('_serialize', ['tag']);
-    }
-
-    /**
-     * Add method
-     *
-     * @return \Cake\Network\Response|void Redirects on successful add, renders view otherwise.
-     */
-    public function add()
-    {
-        $tag = $this->Tags->newEntity();
-        if ($this->request->is('post')) {
-            $tag = $this->Tags->patchEntity($tag, $this->request->data);
-            if ($this->Tags->save($tag)) {
-                $this->Flash->success(__('The tag has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The tag could not be saved. Please, try again.'));
-            }
-        }
-        $files = $this->Tags->Files->find('list', ['limit' => 200]);
-        $notes = $this->Tags->Notes->find('list', ['limit' => 200]);
-        $posts = $this->Tags->Posts->find('list', ['limit' => 200]);
-        $projects = $this->Tags->Projects->find('list', ['limit' => 200]);
-        $this->set(compact('tag', 'files', 'notes', 'posts', 'projects'));
-        $this->set('_serialize', ['tag']);
-    }
-
-    /**
-     * Edit method
-     *
-     * @param string|null $id Tag id.
-     * @return \Cake\Network\Response|void Redirects on successful edit, renders view otherwise.
-     * @throws \Cake\Network\Exception\NotFoundException When record not found.
-     */
-    public function edit($id = null)
-    {
-        $tag = $this->Tags->get($id, [
-            'contain' => ['Files', 'Notes', 'Posts', 'Projects']
-        ]);
-        if ($this->request->is(['patch', 'post', 'put'])) {
-            $tag = $this->Tags->patchEntity($tag, $this->request->data);
-            if ($this->Tags->save($tag)) {
-                $this->Flash->success(__('The tag has been saved.'));
-
-                return $this->redirect(['action' => 'index']);
-            } else {
-                $this->Flash->error(__('The tag could not be saved. Please, try again.'));
-            }
-        }
-        $files = $this->Tags->Files->find('list', ['limit' => 200]);
-        $notes = $this->Tags->Notes->find('list', ['limit' => 200]);
-        $posts = $this->Tags->Posts->find('list', ['limit' => 200]);
-        $projects = $this->Tags->Projects->find('list', ['limit' => 200]);
-        $this->set(compact('tag', 'files', 'notes', 'posts', 'projects'));
-        $this->set('_serialize', ['tag']);
     }
 
     /**
